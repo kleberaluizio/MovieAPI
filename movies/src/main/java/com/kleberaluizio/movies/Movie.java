6604d5d0245cb035185d9a15d6b6dfffcbc51ba9
kleberaluizio <kleberaluizio@gmail.com>
2023-08-17 21:50:38 -0300

    Adding last changes

---
+++ movies/src/main/java/com/kleberaluizio/movies/Movie.java
@@ -0,0 +1,32 @@
+package com.kleberaluizio.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // This annotation takes care of all getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference // It will stores only ids, the Review Object will be in another collection
+    private List<Review> reviewsIds;
+
+
+}
---
+++ movies/src/main/java/com/kleberaluizio/movies/Review.java
@@ -0,0 +1,18 @@
+package com.kleberaluizio.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviews")
+@Data // This annotation takes care of all getters and setters
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+}
