fbe3db75ff1e37dc14b644ec2a5e1da7316441ab
kleberaluizio <kleberaluizio@gmail.com>
2023-09-30 13:44:20 -0300

    Adding new features

--- MovieClient/movie-gold-v1/src/App.js
+++ MovieClient/movie-gold-v1/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
@@ -13,7 +12,7 @@ function App() {
     
       const response = await api.get("/api/v1/movies");
 
-      console.log(response.data);
+      console.log(response.data);  
 
       setMovies(response.data);
 
